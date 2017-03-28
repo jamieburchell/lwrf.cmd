4556a36a6bb40d67a72995136d15fcd654a96812
Jamie Burchell <jamieburchell@users.noreply.github.com>
2017-03-28 21:29:37 +0100

    Bump to version 7
    Support open, close and stop relay switches
    Support unlinking TRV and Thermostats
    Support colour changing and cycling some supported LED lighting
    Renamed "register" to "link"
    Renamed "--cancel-all" to "cancel-all"

--- lwrf.cmd
+++ lwrf.cmd
@@ -13,15 +13,15 @@ set debug=0
 :: -------------------------------------------------
 
 :: -- Room configuration ---------------------------
-:: Add as many rooms as necessary (max 16)
+:: Add as many rooms as necessary (max 15)
 :: E.g:
 :: R1=spare bedroom
 :: R2=living-room
 :: R3=office
-set R1=bedroom1
+set R1=spare-bedroom
 set R2=landing
 set R3=bathroom
-set R4=bedroom2
+set R4=master-bedroom
 set R5=sitting-room
 set R6=lounge
 set R7=office
@@ -29,7 +29,7 @@ set R8=kitchen
 :: -------------------------------------------------
 
 :: -- Room/Device configuration --------------------
-:: Add as many devices as necessary (max 16 per room)
+:: Add as many devices as necessary (max 15 per room)
 :: see below for TRVs and thermostats
 :: E.g:
 :: R1D1=lights
@@ -67,7 +67,7 @@ set R4M1=relax
 :: -------------------------------------------------
 
 :: -- TRV/Stat configuration -----------------------
-:: Add as many TRVs/Stats as necessary (max 16)
+:: Add as many TRVs/Stats as necessary (max 80)
 :: Each TRV/stat must be on a different number
 :: E.g:
 :: TRV1=lounge
@@ -76,6 +76,10 @@ set R4M1=relax
 :: STAT4=hall
 :: TRV5=kitchen
 set STAT1=main
+set TRV2=master-bedroom
+set TRV3=lounge
+set TRV4=kitchen
+set TRV5=spare-bedroom
 :: -------------------------------------------------
 
 :: /////////////////////////////////////////////////
@@ -96,16 +100,16 @@ if errorlevel 1 echo ncat not found 1>&2 && goto err
 
 set display=%~1^|%~2 %~3
 
-if "%~1"=="register" (
+if "%~1"=="link" (
   set function=F*p
-  set display=register
+  set display=link
   echo Accept pairing request at WifiLink unit
   goto send
 )
 
 if "%~1"=="seq" (
   if "%~2"=="" goto usage
-  if "%~2"=="--cancel-all" (
+  if "%~2"=="cancel-all" (
     set function=FcP"*"
     set display=cancel all^|sequences
   ) else (
@@ -119,18 +123,21 @@ if "%~1"=="trv" (
 
   if "%~2"=="" goto usage
 
-  for /l %%r in (1,1,16) do (
+  for /l %%r in (1,1,80) do (
+
     if "!TRV%%r!"=="%~2" (
 
       set room=R%%r
       set display=trv^|%~3
 
-      if "%~3"=="register" set function=F*L& goto send
+      if "%~3"=="link"   set function=F*L& goto send
+      if "%~3"=="unlink" set function=F*xU& goto send
 
       set device=Dh
 
       if "%~3"=="off" set function=F*tP50.0& goto send
       if "%~3"=="on"  set function=F*tP60.0& goto send
+
       if "%~3"=="temp" (
         if "%~4"=="" goto usage
         set temp=%~4
@@ -140,6 +147,7 @@ if "%~1"=="trv" (
         set display=!display! !temp!
         goto send
       )
+
       if "%~3"=="pos" (
         if "%~4"=="" goto usage
         set pos=%~4
@@ -153,22 +161,25 @@ if "%~1"=="trv" (
       goto usage
     )
   )
-
   goto usage
 )
 
 if "%~1"=="stat" (
-
   if "%~2"=="" goto usage
-
-  for /l %%r in (1,1,16) do (
+  for /l %%r in (1,1,80) do (
     if "!STAT%%r!"=="%~2" (
 
       set room=R%%r
 
-      if "%~3"=="register" (
+      if "%~3"=="link" (
         set function=F*L
-        set display=thermostat^|register
+        set display=thermostat^|link
+        goto send
+      )
+
+      if "%~3"=="unlink" (
+        set function=F*xU
+        set display=thermostat^|unlink
         goto send
       )
 
@@ -195,6 +206,7 @@ if "%~1"=="stat" (
         if "%~4"=="away"     set function=F*mP2& goto send
         if "%~4"=="frost"    set function=F*mP3& goto send
         if "%~4"=="constant" set function=F*mP4& goto send
+
         if "%~4"=="holiday" (
           set days=%~5
           if !days! lss 0  goto usage
@@ -212,7 +224,7 @@ if "%~1"=="stat" (
 
 if "%~1"=="" goto usage
 
-for /l %%r in (1,1,16) do (
+for /l %%r in (1,1,15) do (
   if "!R%%r!"=="%~1" (
 
     set room=R%%r
@@ -233,7 +245,8 @@ for /l %%r in (1,1,16) do (
 
     if "%~2"=="" goto usage
 
-    for /l %%d in (1,1,16) do if "!R%%rD%%d!"=="%~2" set device=D%%d
+    for /l %%d in (1,1,15) do if "!R%%rD%%d!"=="%~2" set device=D%%d
+
     if "!device!"=="" goto usage
 
     if "%~3"=="on"        set function=F1& goto send
@@ -241,6 +254,10 @@ for /l %%r in (1,1,16) do (
     if "%~3"=="lock"      set function=Fl& goto send
     if "%~3"=="full-lock" set function=Fk& goto send
     if "%~3"=="unlock"    set function=Fu& goto send
+    if "%~3"=="open"      set function=F^(& goto send
+    if "%~3"=="close"     set function=F^)& goto send
+    if "%~3"=="stop"      set function=F^^& goto send
+
     if "%~3"=="dim" (
       if "%~4"=="" goto usage
       set dimlevel=%~4
@@ -250,6 +267,36 @@ for /l %%r in (1,1,16) do (
       set function=FdP!dimlevel!
       set display=!display! %~4%%
       goto send
+    )
+
+    if "%~3"=="colour" (
+
+      if "%~4"=="" goto usage
+
+      set display=!display! %~4
+
+      if "%4"=="white"              set function=F*cP1& goto send
+      if "%4"=="green-white"        set function=F*cP2& goto send
+      if "%4"=="red-white"          set function=F*cP3& goto send
+      if "%4"=="yellow-white"       set function=F*cP4& goto send
+      if "%4"=="red"                set function=F*cP5& goto send
+      if "%4"=="middle-red"         set function=F*cP6& goto send
+      if "%4"=="pink"               set function=F*cP7& goto send
+      if "%4"=="light-red"          set function=F*cP8& goto send
+      if "%4"=="orange"             set function=F*cP9& goto send
+      if "%4"=="middle-orange"      set function=F*cP10& goto send
+      if "%4"=="green-white"        set function=F*cP11& goto send
+      if "%4"=="light-orange"       set function=F*cP12& goto send
+      if "%4"=="green"              set function=F*cP13& goto send
+      if "%4"=="middle-green"       set function=F*cP14& goto send
+      if "%4"=="light-yellow-green" set function=F*cP15& goto send
+      if "%4"=="yellow-green"       set function=F*cP16& goto send
+      if "%4"=="blue"               set function=F*cP17& goto send
+      if "%4"=="water-blue"         set function=F*cP18& goto send
+      if "%4"=="light-blue"         set function=F*cP19& goto send
+      if "%4"=="white-blue"         set function=F*cP20& goto send
+      if "%4"=="cycle"              set function=F*y& goto send
+      goto usage
     )
   )
 )
@@ -277,20 +324,21 @@ if "!rx_msg!"=="" echo No confirmation received from WifiLink 1>&2
 goto err
 
 :usage
-set usage=LightwaveRF Windows Command Line Control v6.6 by Jamie Burchell!lf!^
+set usage=LightwaveRF Windows Command Line Control v7.0 by Jamie Burchell!lf!^
 
 Usage:!lf!^
-lwrf register!lf!^
-lwrf room-name device-name on^|off^|lock^|full-lock^|unlock^|dim 1-100!lf!^
+lwrf link!lf!^
+lwrf room-name device-name on^|off^|open^|close^|stop^|lock^|full-lock^|unlock^|dim 1-100^|colour [colour-name^|cycle]!lf!^
 lwrf room-name off!lf!^
 lwrf room-name mood mood-name!lf!^
-lwrf trv trv-name register^|on^|off^|temp 0-40^|pos 0-5!lf!^
-lwrf stat stat-name register^|temp 0-40^|mode [standby^|running^|away^|frost^|constant^|holiday 1-90]!lf!^
+lwrf trv trv-name link^|unlink^|on^|off^|temp 0-40^|pos 0-5!lf!^
+lwrf stat stat-name link^|unlink^|temp 0-40^|mode [standby^|running^|away^|frost^|constant^|holiday 1-90]!lf!^
 lwrf seq sequence-name!lf!^
-lwrf seq --cancel-all!lf!^
+lwrf seq cancel-all!lf!^
 
 Examples:!lf!^
 lwrf lounge lights dim 50!lf!^
+lwrf lounge lights colour red!lf!^
 lwrf lounge mood relax!lf!^
 lwrf "sitting room" "wall lights" on!lf!^
 lwrf seq "my sequence"!lf!^
